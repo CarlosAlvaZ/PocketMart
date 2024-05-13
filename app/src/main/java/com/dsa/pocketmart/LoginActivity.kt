009fbff4cf08ae6761e970372165907ef36673ac
= <=>
2024-05-12 23:20:51 -0600

    Added authentication

--- app/src/main/java/com/dsa/pocketmart/LoginActivity.kt
+++ app/src/main/java/com/dsa/pocketmart/LoginActivity.kt
@@ -1,20 +1,64 @@
 package com.dsa.pocketmart
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.dsa.pocketmart.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.hook.setOnClickListener {
+            navigateToSignup()
         }
+
+        binding.button.setOnClickListener {
+            loginAction()
+        }
+    }
+
+    private fun loginAction() {
+        val email = binding.email.text.toString()
+        val password = binding.password.text.toString()
+
+        if (email.isNotEmpty() && password.isNotEmpty()) {
+            firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
+                if (it.isSuccessful) {
+                    navigateToMainActivity()
+                } else {
+                    Toast.makeText(this@LoginActivity, it.exception.toString(), Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
+        } else {
+            Toast.makeText(
+                this@LoginActivity, getString(R.string.llenar_campos), Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    private fun navigateToSignup() {
+        val intent = Intent(this@LoginActivity, SignupActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun navigateToMainActivity() {
+        val intent = Intent(this@LoginActivity, MainActivity::class.java)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/dsa/pocketmart/SignupActivity.kt
@@ -0,0 +1,70 @@
+package com.dsa.pocketmart
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.dsa.pocketmart.databinding.ActivitySignupBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class SignupActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignupBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.hook.setOnClickListener {
+            navigateToLogin()
+        }
+
+        binding.button.setOnClickListener {
+            signupAction()
+        }
+    }
+
+    private fun signupAction() {
+        val email = binding.email.text.toString()
+        val password = binding.password.text.toString()
+        val passwordConfirmation = binding.passwordConfirmation.text.toString()
+
+        if (email.isNotEmpty() && password.isNotEmpty() && passwordConfirmation.isNotEmpty()) {
+            if (password == passwordConfirmation) {
+                firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        navigateToLogin()
+                    } else {
+                        Toast.makeText(
+                            this@SignupActivity, it.exception.toString(), Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+            } else {
+                Toast.makeText(
+                    this@SignupActivity,
+                    getString(R.string.contrasenas_deben_coincidir),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        } else {
+            Toast.makeText(
+                this@SignupActivity, getString(R.string.llenar_campos), Toast.LENGTH_SHORT
+            ).show()
+        }
+
+    }
+
+    private fun navigateToLogin() {
+        val intent = Intent(this@SignupActivity, LoginActivity::class.java)
+        startActivity(intent)
+    }
+}
